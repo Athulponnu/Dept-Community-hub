be92660ddf45c6c2e5ae1e67e0aaa3471d8908ab
Athul <152899418+Athulponnu@users.noreply.github.com>
2026-03-16 11:22:43 +0530

    env avoided

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  "https://gsgtbcnajbfocwyzvdjg.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzZ3RiY25hamJmb2N3eXp2ZGpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzOTE0NTMsImV4cCI6MjA4ODk2NzQ1M30.-cLKr-_77h7yWqPh12eFalvEVtmBDsU7M39sOgMzgqg"  // paste your full anon key here
-)
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
